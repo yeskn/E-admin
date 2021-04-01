5134564e0fca63f7710002092b92b8ad39ac42c8
maple <598236556@qq.com>
2021-04-01 11:40:12 +0800

    状态码判断是否为空

--- src/traits/ApiJson.php
+++ src/traits/ApiJson.php
@@ -47,25 +47,42 @@ trait  ApiJson
         throw new HttpResponseException($response);
     }
 
-    /**
-     * 输出自定义json
-     * @Author: rocky
-     * 2019/7/12 17:08
-     * @param array $data 数据
-     * @param int $code 错误码
-     * @param string $errMsg 错误信息
-     * @param int $http_code 状态码
-     */
-    protected function responseJsonData($data = [], $code = 200, $errMsg = '', $http_code = 200)
-    {
-        $return['code'] = (int)$code;
-        if (!empty($errMsg)) {
-            $return['message'] = $errMsg;
-        } else {
-            $message           = isset(config('apiCode')[$code]) ? config('apiCode')[$code] : '';
-            $return['message'] = $message;
-        }
-        $return['data'] = $data;
-        return json($return, $http_code);
-    }
+	/**
+	 * 输出自定义json
+	 * @Author: rocky
+	 * 2019/7/12 17:08
+	 * @param array $data 数据
+	 * @param int $code 错误码
+	 * @param string $errMsg 错误信息
+	 * @param int $http_code 状态码
+	 */
+	protected function responseJsonData($data = [], $code = 200, $errMsg = '', $http_code = 200)
+	{
+		// 判断是否为空数组
+		$code = $this->validates($data, $code);
+		$return['code'] = (int)$code;
+		if (!empty($errMsg)) {
+			$return['message'] = $errMsg;
+		} else {
+			$message           = isset(config('apiCode')[$code]) ? config('apiCode')[$code] : '';
+			$return['message'] = $message;
+		}
+		$return['data'] = $data;
+		return json($return, $http_code);
+	}
+
+	/**
+	 * 判断是否是空数组/集合
+	 * @param mixed $data 数据
+	 * @param int $code 状态码
+	 * @return int
+	 */
+	protected function validates($data, $code)
+	{
+		if ((is_string($data) && empty($data)) ||
+			count($data) < 1) {
+			$code = 4004;
+		}
+		return $code;
+	}
 }
