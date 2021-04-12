410faccac2d641fd02057d8237ee7b70fdf1871b
rocky <631670747@qq.com>
2021-04-12 11:48:10 +0800

    select下拉框options选项不存在清空值

--- src/resources/src/components/select.vue
+++ src/resources/src/components/select.vue
@@ -5,18 +5,20 @@
 <script>
     import {defineComponent,watch,ref} from "vue";
     import request from '@/utils/axios'
+    import {findTree} from '@/utils'
     export default defineComponent({
         name: "EadminSelect",
         props:{
             params: Object,
             modelValue:[Object,Array,String,Number],
-            loadOption:[Object,Array,String,Number],
+            loadOptionField:[Object,Array,String,Number],
             loadField:[Object,Array,String,Number],
             options:[Object,Array,String,Number],
         },
         emits:['update:modelValue','update:loadField','update:loadOptionField'],
         setup(props,ctx){
             const value = ref(props.modelValue)
+            let loadFieldValue = props.loadField
             watch(()=>props.modelValue,val=>{
                 value.value =val
                 changeHandel(val,true)
@@ -24,17 +26,23 @@
             watch(value,value=>{
                 ctx.emit('update:modelValue',value)
             })
+            if(!findTree(props.options,value.value,'id')){
+                value.value = ''
+                loadFieldValue = ''
+            }
             changeHandel(value.value,false)
-            function changeHandel(value,reset=true) {
+            function changeHandel(val,reset=true) {
                 if(props.params){
                     request({
                         url: '/eadmin.rest',
                         method:'post',
-                        params: Object.assign(props.params, {eadminSelectLoad: true, eadmin_id: value}),
+                        params: Object.assign(props.params, {eadminSelectLoad: true, eadmin_id: val}),
                     }).then(res=>{
                         ctx.emit('update:loadOptionField',res.data)
                         if(reset){
                             ctx.emit('update:loadField','')
+                        }else{
+                            ctx.emit('update:loadField',loadFieldValue)
                         }
                     })
                 }
