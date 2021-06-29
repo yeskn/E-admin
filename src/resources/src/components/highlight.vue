f2f4a78802cd7f215d4ff07527746668ea3564c9
rocky <631670747@qq.com>
2021-06-29 10:17:46 +0800

    highlight组件

---
+++ src/resources/src/components/highlight.vue
@@ -0,0 +1,12 @@
+<template>
+    <pre v-highlight><code>{{code}}</code></pre>
+</template>
+
+<script>
+    export default {
+        name: "highlight",
+        props:{
+            code:String,
+        }
+    }
+</script>
