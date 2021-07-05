54652af10a65358c114a43ea651dbb1cb47bdd00
邬沛延 <mapleleaf12138@gmail.com>
2021-07-05 10:09:26 +0800

    ElLink新增target链式

--- src/component/basic/Link.php
+++ src/component/basic/Link.php
@@ -21,8 +21,19 @@ class Link extends Component
     protected $name = 'ElLink';
     public function __construct($content)
     {
-        $this->content($content);
+        $this->content("<span>{$content}</span>");
     }
+
+	/**
+	 * 打开方式
+	 * @param string $attr _blank(在新窗口中打开) / _self(在相同的窗口打开) / _parent(在父窗口打开) / _top(在整个窗口中)
+	 * @return $this
+	 */
+	public function target($attr = '_blank')
+	{
+		$this->attr('target', $attr);
+		return $this;
+	}
 
     public static function create($content)
     {
